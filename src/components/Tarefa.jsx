b6c6d35c78c26efa258db6da7f9b04538a377038
leticia-chijo <leticia.chijo@gmail.com>
2024-03-22 18:05:05 -0300

    Aula - Props no Styled Component

--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export default function Tarefa({tarefas, tarefa, terminadas, setTerminadas}) {
 
     function terminarTarefa(tarefaTerminada) {
@@ -12,12 +14,38 @@ export default function Tarefa({tarefas, tarefa, terminadas, setTerminadas}) {
     }
 
     return (
-        <li
-            className={`tarefa ${terminadas.includes(tarefa) ? "finalizada" : ""} `}
+        <ItemLista
             onClick={() => terminarTarefa(tarefa)}
+            finalizada={terminadas.includes(tarefa)}
         >
             <ion-icon name={terminadas.includes(tarefa) ? "checkmark-circle" : "ellipse-outline"}></ion-icon>
             {tarefa}
-        </li>
+        </ItemLista>
     );
 }
+
+const ItemLista = styled.li`
+    color: ${props => props.finalizada ? "#aaaaaa" : "#303f4e"};
+    text-decoration: ${props => props.finalizada ? "line-through" : ""};
+    padding: 22px;
+    display: flex;
+    align-items: center;
+    font-size: 22px;
+    cursor: pointer;
+    background-color: #f3f3f3;
+    &:hover {
+        color: #21c0a8;
+    }
+    &:not(:last-child) {
+        border-bottom: 1px solid #e7e7e7;
+    }
+    ion-icon {
+        flex-shrink: 0;
+        font-size: 40px;
+        margin-right: 16px;
+        ${props => props.finalizada ? "#78dbad" : "#303f4e"};
+    }
+    &:hover ion-icon {
+        color: #21c0a8;
+    }
+`
